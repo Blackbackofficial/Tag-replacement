791c49dc4bac11cf6af8a78bcdb4e098b08ae993
Chernov Ivan <51726661+Blackblackofficial@users.noreply.github.com>
2020-06-07 21:41:29 +0300

    Основной залив

---
+++ WindowsFormsApp8/WindowsFormsApp8/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace WindowsFormsApp8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Genesis";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 99);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 50);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "WinCC";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1030, 22);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(122, 58);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Закрыть";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(823, 146);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(175, 45);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Замена тегов";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1029, 97);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(130, 69);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Сохранить как";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 231);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(481, 260);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(12, 544);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(481, 393);
+            this.richTextBox2.TabIndex = 6;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.AutoWordSelection = true;
+            this.richTextBox3.Location = new System.Drawing.Point(527, 231);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(632, 706);
+            this.richTextBox3.TabIndex = 7;
+            this.richTextBox3.Text = "";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(397, 40);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(618, 100);
+            this.richTextBox4.TabIndex = 8;
+            this.richTextBox4.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(699, 161);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Замена тегов:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 199);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(253, 20);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Отображение объектов Genesis";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 511);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(243, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Отображение объектов WinCC";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(580, 199);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(239, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Отображение новых объектов";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 67);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(373, 26);
+            this.textBox1.TabIndex = 13;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 155);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(373, 26);
+            this.textBox2.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(490, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(238, 20);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Какой тег на какой заменился";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(132, 38);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(243, 20);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Задание пути к файлу Genesis";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(132, 129);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(233, 20);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Задание пути к файлу WinCC";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1181, 966);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ WindowsFormsApp8/WindowsFormsApp8/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                 richTextBox1.Text = File.ReadAllText(openFile.FileName);
+                textBox1.Text = openFile.FileName;     
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = openFile.FileName;
+                richTextBox2.Text = File.ReadAllText(openFile.FileName);        
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // заносим файл Genesis в массив строк
+            string path1 = textBox1.Text;
+                    string[] fFile = File.ReadAllLines(path1);
+           
+            // заносим файл WinCC в массив строк
+            string path2 = textBox2.Text;     
+            string[] sFile = File.ReadAllLines(path2);
+          
+            int Fline_1 = fFile.Length;
+            int k = 0;
+            int Fline_2 = sFile.Length;
+            int mas = 0;
+           
+            // считаем кол-во строк с тегами Genesis
+            for (int i = 0; i < Fline_1; i++)
+            {
+                if (fFile[i].Contains("AK"))
+                {
+                    mas = mas + 1;
+                }
+               
+            }
+           // создаем и заполняем массив тегами из Genesis
+            string[] arr = new string[mas];
+            for (int o = 0; o < mas; o++)
+            {
+                for (int p = k; p < Fline_1; p++)
+                {
+                    if (fFile[p].Contains("AK"))
+                    {
+                        arr[o] = fFile[p];
+                        k = p + 1;
+                        break;
+                    }
+                }
+               
+            }
+ //int qwe = 0;
+      
+            // начинаем сравнивать теги файла WinCC с тегами файла Genesis
+            for (int i = 0; i <10000; i++)
+            {
+
+                if (sFile[i].Contains("ak.") || sFile[i].Contains("тэг"))
+                {
+                    // копируем строку с тегом WinCC в пустую строку для сравнения
+                    string text1 = sFile[i];
+                 
+                 //   text1 = text1.ToLower();                 
+                    //text1 = text1.Replace(" ", "");
+                      text1 = text1.Substring(text1.Length - 48);
+                 
+                   // richTextBox4.Text += text1 + Environment.NewLine;     
+                    richTextBox4.Text += sFile[i] + Environment.NewLine;
+                    double max = 0;
+                   int k1 = 0;
+                 //   double count4 = 0;
+                    for (int j = 0; j < mas; j++)
+                    {
+                        
+                        if (arr[j].Contains("AK."))
+                        {
+                            // копируем строку с тегом Genesis в пустую строку для сравнения
+                            string text2 = arr[j];
+              
+                      //      text2 = text2.ToLower();                          
+                           // text2 = text2.Replace(" ", ""); 
+                           text2 = text2.Substring(text2.Length - 48); 
+                           
+                           // richTextBox4.Text += text2 + Environment.NewLine;
+
+                            // апеременные для подсчета каждого символа в строках
+                            double count1 = 0; // для тега WinCC
+                            double count2 = 0; // для тега Genesis
+                            double count3 = 0; // подсчет общих символов в обеих строках
+                            char PreCh = '\\';
+                            double t1 = text1.Length;
+                            double t2 = text2.Length;
+                           
+                            // сравнение строк посимвольно
+                            foreach (char ch in text1)
+                            {
+                                foreach (char cha in text1)
+
+                                    if (cha != PreCh && cha == ch)
+                                        count1++;
+
+                                foreach (char cha2 in text2)
+
+                                    if (cha2 != PreCh && cha2 == ch)
+                                        count2++;
+                                if (count1 <= count2)
+                                {
+                                    count3 = count3 + count1;
+                                }
+                                else
+                                {
+                                    count3 = count3 + count2;
+                                }
+                                if (count1 != 0)
+                                {
+
+                                    count1 = 0;
+                                    // PreCh = ch;
+                                }
+                                if (count2 != 0)
+                                {
+                                    //count1 = 0;
+                                    count2 = 0;
+                                    PreCh = ch;
+
+                                    int l = 0;
+                                    var tmp = text2[l].ToString();
+                                    text2 = text2.Replace(tmp, "");
+                                    //text2 = text2.Insert(i, tmp);
+                                    l++;
+                                    if (text2.Length - 1 < l)
+                                    {
+                                        break;
+                                    }
+
+                                }
+                                Application.DoEvents();
+
+                            }
+                            double z = 0;
+                            z = count3 / (t1 + t2 - count3); // коэф-ент схожести строк. От 0 до 1
+                         //   count4 = count3;
+                           // int k1 = 0;
+                            if (max < z && z < 1.0)
+                            {
+                                max = z;
+                                k1 = j;
+                               
+                            }                          
+                        }        
+                        if (j == mas - 1)  //&& max > 0.95
+                        {
+                            // qwe = qwe + 1;
+                            // richTextBox4.Text += qwe;    
+                           // string test = arr[k1].Substring(arr[k1].Length - 48);
+                            richTextBox4.Text += arr[k1] + Environment.NewLine;  
+                            //richTextBox4.Text += max + Environment.NewLine;
+                        //    richTextBox4.Text += count4 + Environment.NewLine;
+                            richTextBox4.Text += "-------------------" + Environment.NewLine;
+                           
+                            sFile[i] = sFile[i].Replace(sFile[i], arr[k1]);                       
+                            arr[k1] = arr[k1].Replace(arr[k1], "11");
+                                         
+                            max = 0;
+                            k1 = 0;
+                        }
+                    }
+
+                    //-----------------------------------------------------------
+
+                    //-------------------------------------------------------------
+                   
+                }
+               
+            if (i == 9999)
+                { 
+                   richTextBox3.Lines = sFile; 
+                }  
+                Application.DoEvents();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog savefile = new SaveFileDialog();
+            savefile.DefaultExt = ".txt";
+            savefile.Filter = "Test files|*.txt";
+            if (savefile.ShowDialog() == System.Windows.Forms.DialogResult.OK && savefile.FileName.Length > 0)
+            {
+                using (StreamWriter sw = new StreamWriter(savefile.FileName, true))
+                {
+                    sw.WriteLine(richTextBox3.Text);
+                    sw.Close();
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
